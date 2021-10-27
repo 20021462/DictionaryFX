067cd4c2a94ccb4046dff73c407c3b3e4a294c57
LnG <pleasewaitlong@gmail.com>
2021-10-27 17:15:47 +0700

    sort

--- src/main/java/model/Dictionary.java
+++ src/main/java/model/Dictionary.java
@@ -15,15 +15,35 @@ public class Dictionary {
 
     public void add(String wordTarget, String wordSound, String wordExplain) {
         wordExplain=wordExplain.concat("\n");
-        Word value=new Word(wordTarget,wordSound,wordExplain);
-        words.add(value);
-        for (int i=words.size()-1;i>0;i--){
-            if (value.getWordTarget().compareTo(words.get(i-1).getWordTarget())<0){
-                words.set(i,words.get(i-1));
-            } else {
-                words.set(i,value);
-                break;
+        words.add(new Word(wordTarget,wordSound,wordExplain));
+        sort(this.getWords().size()-1);
+    }
+
+    public int sort(int index){
+        Word value=this.getWords().get(index);
+        int i;
+        if (index == this.getWords().size()-1||(index>0&&value.getWordTarget().compareTo(this.getWords().get(index-1).getWordTarget())<0)){
+            for (i=index;i>0;i--){
+                if (value.getWordTarget().compareTo(words.get(i-1).getWordTarget())<0){
+                    words.set(i,words.get(i-1));
+                } else {
+                    words.set(i,value);
+                    return i;
+                }
             }
+            words.set(i,value);
+            return i;
+        } else {
+            for (i=index;i<words.size()-1;i++){
+                if (value.getWordTarget().compareTo(words.get(i+1).getWordTarget())>0){
+                    words.set(i,words.get(i+1));
+                } else {
+                    words.set(i,value);
+                    return i;
+                }
+            }
+            words.set(i,value);
+            return i;
         }
     }
 
